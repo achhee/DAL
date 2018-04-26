47ca785eea7c8d9795a00839e9f0a872916e67a1
MUHAMMAD ASIM NAEEM <MUHAMMADASIM.NAEEM@wdc.com>
2018-04-26 13:28:52 +0800

    Add project files.

---
+++ Enums/Database.cs
@@ -0,0 +1,48 @@
+﻿// ***********************************************************************
+// Assembly         : WD.DataAccess
+// Author           : Asim_n
+// Created          : 03-14-2017
+//
+// Last Modified By : Asim_n
+// Last Modified On : 04-28-2017
+// ***********************************************************************
+// <copyright file="Database.cs" company="Western Digital">
+//     Copyright © Western Digital 2017
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+// namespace: WD.DataAccess.Enums
+//
+// summary:	.
+
+
+namespace WD.DataAccess.Enums
+{
+    
+    /// <summary>   A databases. </summary>
+    ///
+    /// <remarks>   Asim Naeem, 7/20/2017. </remarks>
+    
+
+    public class Databases
+    {
+       
+       /// <summary>    Default constructor. </summary>
+       ///
+       /// <remarks>    Asim Naeem, 7/20/2017. </remarks>
+       
+
+       public Databases() { }
+       /// <summary>    The BR flag. </summary>
+       public const int BR = 1;
+       /// <summary>    The TX flag. </summary>
+       public const int TX = 2;
+    }
+}
---
+++ Enums/Transaction.cs
@@ -0,0 +1,52 @@
+﻿// ***********************************************************************
+// Assembly         : WD.DataAccess
+// Author           : Asim_n
+// Created          : 03-24-2017
+//
+// Last Modified By : Asim_n
+// Last Modified On : 07-19-2017
+// ***********************************************************************
+// <copyright file="Transaction.cs" company="Western Digital">
+//     Copyright © Western Digital 2017
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+// namespace: WD.DataAccess.Enums
+//
+// summary:	.
+
+
+namespace WD.DataAccess.Enums
+{
+    
+    /// <summary>   A transaction. </summary>
+    ///
+    /// <remarks>   Asim Naeem, 7/20/2017. </remarks>
+    
+
+    public class Transaction 
+    {
+        
+        /// <summary>   Default constructor. </summary>
+        ///
+        /// <remarks>   Asim Naeem, 7/20/2017. </remarks>
+        
+
+        public Transaction() { }
+      /// <summary> No Transaction for Database. </summary>
+      public const int  None = 1;
+      /// <summary> Transaction for Connections Separately. </summary>
+      public const int Yes = 2;
+       /// <summary>    One Transaction for All Connections without connection Transaction. </summary>
+       public const int  TS = 3;
+        /// <summary>   One Transaction for All Connections with connection Transaction. </summary>
+        public const int TSNT = 4;
+    }
+}
